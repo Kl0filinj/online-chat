5ceda2353bef8088ebf8e1846fa3ccb9b91cd4d3
Velychko Daniil <101584452+Kl0filinj@users.noreply.github.com>
2023-02-22 22:25:43 +0100

    Update socketConnection.js

--- src/utils/socketConnection.js
+++ src/utils/socketConnection.js
@@ -1,5 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io.connect('http://localhost:3000');
+const socket = io.connect('https://online-chat-server.onrender.com');
 // https://online-chat-server.onrender.com
+// http://localhost:3000
 export default socket;
