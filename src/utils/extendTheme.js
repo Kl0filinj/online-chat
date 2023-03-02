eff8ca63f8a927d80b21b222ed09f02d1fa15961
Velychko Daniil <101584452+Kl0filinj@users.noreply.github.com>
2023-03-02 19:15:50 +0100

    change color mode settings

--- src/utils/extendTheme.js
+++ src/utils/extendTheme.js
@@ -1,7 +1,7 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
-  config: { initialColorMode: 'dark', useSystemColorMode: true },
+  config: { initialColorMode: 'dark', useSystemColorMode: false },
   fonts: {
     heading: `'Inter', sans-serif`,
   },
